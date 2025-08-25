b31995cfeee082acc8298f0f4f7a3a65e6cbb247
NeetGupta <neetgupta702@gmail.com>
2025-08-25 22:38:27 +0530

    Update main.tf

--- terraform-azure-web-app/modules/resource_group/main.tf
+++ terraform-azure-web-app/modules/resource_group/main.tf
@@ -1,26 +1,26 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "AzureDevops" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
 resource "azurerm_storage_account" "AzureDevops" {
-  name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  name                     = var.Stroge_account_name
+  resource_group_name      = azurerm_resource_group.AzureDevops.name
+  location                 = azurerm_resource_group.AzureDevops.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "example" {
+resource "azurerm_storage_container" "AzureDevops" {
   name                  = "content"
-  storage_account_id    = azurerm_storage_account.example.id
+  storage_account_id    = azurerm_storage_account.AzureDevops.id
   container_access_type = "private"
 }
 
-resource "azurerm_storage_blob" "example" {
+resource "azurerm_storage_blob" "Azuredevops" {
   name                   = "my-awesome-content.zip"
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
+  storage_account_name   = azurerm_storage_account.AzureDevops.name
+  storage_container_name = azurerm_storage_container.Azuredevops.name
   type                   = "Block"
   source                 = "some-local-file.zip"
 }
