361a008d1b83ed38c52cc0b40a1066a3a971f523
NeetGupta <neetgupta702@gmail.com>
2025-08-25 22:38:35 +0530

    Create Variables.tf

---
+++ terraform-azure-web-app/modules/resource_group/Variables.tf
@@ -0,0 +1,16 @@
+variables "resource_group_name"{
+ description = "this is resource group name"
+    type = string
+    default = "Azure Devops"
+}
+variable "resource_group_location{
+description = "this is resource group name"
+    type = string
+    default = "West Europe"
+}
+
+variable "Stroge_account_name" {
+ description = "this is resource group name"
+    type = string
+    default = "Azure Devops"
+}
